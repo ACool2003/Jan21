582ff1572412d9847f380d0cdd47622a7a1b72ad
ACool2003 <157270030+ACool2003@users.noreply.github.com>
2024-01-21 01:33:28 -0500

    Add files via upload

---
+++ SongLyrics.java
@@ -0,0 +1,11 @@
+package second;
+
+public class SongLyrics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println ("Through mostly vacant streets");
+System.out.println ("A baker from the out-skirts of his town");
+	}
+
+}
